2ce53c9de6e69e343ce46dd5e6392708689195b2
Yash Mulik <yashmulik95@gmail.com>
2025-02-16 17:24:43 +0530

    middleware fix

--- middlewares/authOwnerMid.js
+++ middlewares/authOwnerMid.js
@@ -1,22 +1,27 @@
 const jwt = require("jsonwebtoken");
 const Owner = require("../model/UserModel");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 exports.protect = async (req, res, next) => {
-  let token;
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      req.owner = await Owner.findById(decoded.id).select("-password");
-      next();
-    } catch (error) {
-      return res.status(401).json({ message: "Not authorized, token failed" });
-    }
-  }
+  let token = req.headers.authorization?.split(" ")[1];
+  // console.log(token);
+
   if (!token) {
     return res.status(401).json({ message: "Not authorized, no token" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.jwt_key);
+    req.owner = await Owner.findById(decoded.id).select("-password");
+
+    if (!req.owner) {
+      return res.status(401).json({ message: "Owner not found" });
+    }
+
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Not authorized, token failed" });
   }
 };
--- middlewares/authUserMid.js
+++ middlewares/authUserMid.js
@@ -1,16 +1,21 @@
 const jwt = require("jsonwebtoken");
 const User = require("../model/UserModel");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 const authUserMid = async (req, res, next) => {
   let token = req.headers.authorization?.split(" ")[1];
+  console.log(token);
 
   if (!token) {
     return res.status(401).json({ message: "Not authorized, no token" });
   }
 
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findById(decoded.id).select("-password");
+    const decoded = jwt.verify(token, process.env.jwt_key);
+    console.log(decoded);
+    req.user = await User.findById(decoded.userId);
     if (!req.user) {
       return res.status(401).json({ message: "User not found" });
     }
